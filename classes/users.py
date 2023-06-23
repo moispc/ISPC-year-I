d5f8a3d580991060695a4dffab1f5c3eddb5a34b
cerveux <diegomartinguzman@hotmail.com.ar>
2023-06-22 22:57:54 -0300

    conexion
    lista la conexion mysql y la creación de la base de datos

--- classes/users.py
+++ classes/users.py
@@ -1,12 +1,48 @@
 class Usuario:
-    def __init__(self, nombre, contraseña):
-        self.nombre = nombre
-        self.contraseña = contraseña
-        
-    def leer_ley(self):
+    def __init__(self):
+        pass
+    
+    # C - CREATE
+    def create_database(self, connection):
+        # Creación de la base de datos
+        cursor = connection.cursor
+
+
+        cursor.execute('show databases')
+        mysqldbs = connection.cursor.fetchall()
+
+        if ('ispc_lawdb',) in mysqldbs:
+            cursor.execute('USE ispc_lawdb')
+        else:
+            fd = open('bbdd/moispc.sql', 'r')
+            sqlFile = fd.read()
+            fd.close()
+
+            # Separamos todos los comandos de sql
+            sqlCommands = sqlFile.split(';')
+
+            # Execute every command from the input file
+            for command in sqlCommands:
+            # This will skip and report errors
+            # For example, if the tables do not yet exist, this will skip over
+            # the DROP TABLE commands
+                try:
+                    cursor.execute(command)
+                except connection.Error as err:
+                    print("Error de conexión: {0}".format(err))
+    
+    # R - READ
+
+    def leer_ley(self, connection):
         
         print("llamar a una funcion para leer la ley")
+
+
+    def leer_palabra(self, connection):
+        """ muestra la ley dependiendo del numero de ley introducido """
         
+
+    
 
     def ingresar(self):
         print("======================================")
@@ -21,16 +57,17 @@ class Admin(Usuario):
         print("======================================")
     
     
-    def borrar_ley(self):
+    # D - DELETE
+    def borrar_ley(self, connection):
         
         print("llamar a una funcion para borrar una ley")
         
-       
-    def agregar_ley(self):
+    # C - CREATE   
+    def agregar_ley(self, connection):
         
         print("llamar a una funcion para agregar una ley")
         
-        
+    # U - UPDATE
     def modificar_ley(self):
         
         print("llamar a una funcion para modificar una ley")
